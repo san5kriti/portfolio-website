f2888d707781bb0d2855e2fa9355521f47cbad11
sanskriti <sanskriti@sanskritis-MacBook-Pro.local>
2025-03-17 00:35:54 +0000

    🔧 Improve mobile responsiveness on hero section

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,10 +5,10 @@ import Head from "next/head";
 import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-
   return (
     <>
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/initial.png" type="image/png" />
       </Head>
       <Component {...pageProps} />
